4c7e53639e9d00e75f9a41b3c5ac63a26bb96c11
FahimSakib <fsakib991@gmail.com>
2022-11-17 19:23:50 +0600

    navbar

--- resources/js/Pages/pilot.jsx
+++ resources/js/Pages/pilot.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const pilot = () => {
     return (
-        <div>pilot</div>
+        <nav className="rlative container mx-auto p-6">
+            <div className="flex items-center justify-between">
+                <div className="pt-2">
+                    <img src="img/logo.svg" alt="logo" />
+                </div>
+                <div className="hidden md:flex space-x-6">
+                    <a href="#" className="hover:text-blue-600">Pricing</a>
+                    <a href="#" className="hover:text-blue-600">Product</a>
+                    <a href="#" className="hover:text-blue-600">About us</a>
+                    <a href="#" className="hover:text-blue-600">Careers</a>
+                    <a href="#" className="hover:text-blue-600">Community</a>
+                </div>
+                <a href="#" className="hidden md:block p-3 px-6 pt-2 text-white bg-red-700 rounded-full baseline hover:bg-red-500">
+                    Get Started
+                </a>
+            </div>
+        </nav>
     )
 }
 
