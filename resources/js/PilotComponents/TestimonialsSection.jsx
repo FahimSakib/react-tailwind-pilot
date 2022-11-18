96add0a5efd503591bcd8316a118c7ec73124f55
FahimSakib <fsakib991@gmail.com>
2022-11-18 16:20:24 +0600

    testimonials section final

--- resources/js/PilotComponents/TestimonialsSection.jsx
+++ resources/js/PilotComponents/TestimonialsSection.jsx
@@ -18,6 +18,24 @@ const TestimonialsSection = () => {
                 everyone motivated.”
               </p>
             </div>
+            <div className="hidden md:flex flex-col space-y-6 items-center p-6 rounded-lg bg-veryLightGray md:w-1/3">
+              <img src="img/avatar-ali.png" alt="avatar-ali" className="w-16 -mt-14" />
+              <h5 class="text-lg font-bold">Ali Bravo</h5>
+              <p class="text-sm text-darkGrayishBlue">
+                “We have been able to cancel so many other subscriptions since
+                using Manage. There is no more cross-channel confusion and
+                everyone is much more focused.”
+              </p>
+            </div>
+            <div className="hidden md:flex flex-col space-y-6 items-center p-6 rounded-lg bg-veryLightGray md:w-1/3">
+              <img src="img/avatar-richard.png" alt="avatar-richard" className="w-16 -mt-14" />
+              <h5 class="text-lg font-bold">Richard Watts</h5>
+              <p class="text-sm text-darkGrayishBlue">
+                “Manage has supercharged our team’s workflow. The ability to
+                maintain visibility on larger milestones at all times keeps
+                everyone motivated.”
+              </p>
+            </div>
           </div>
         </div>
       </section>
