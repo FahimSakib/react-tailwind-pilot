85817cceb1cb016bc04ea4930c4ecc65ab0ebf2d
FahimSakib <fsakib991@gmail.com>
2022-11-18 19:11:43 +0600

    footer final

--- resources/js/PilotComponents/Footer.jsx
+++ resources/js/PilotComponents/Footer.jsx
@@ -42,6 +42,20 @@ const Footer = () => {
                         <a href="#" class="hover:text-brightRed">Privacy Policy</a>
                     </div>
                 </div>
+                <div className="flex flex-col justify-between">
+                    <form >
+                        <div className="flex space-x-3">
+                            <input type="text" className="flex-1 px-4 rounded-full focus:outline-none"
+                                placeholder="Updated in your inbox" />
+                            <button class="px-6 py-2 text-white rounded-full bg-brightRed hover:bg-brightRedLight focus:outline-none">
+                                Go
+                            </button>
+                        </div>
+                    </form>
+                    <div class="hidden text-white md:block">
+                        Copyright &copy; 2022, All Rights Reserved
+                    </div>
+                </div>
             </div>
         </footer>
     )
