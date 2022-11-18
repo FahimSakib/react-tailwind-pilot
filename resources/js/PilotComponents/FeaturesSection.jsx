a26d8220efa6930409af4902dd29a570f16a595e
FahimSakib <fsakib991@gmail.com>
2022-11-18 10:46:18 +0600

    features section right side item 01

--- resources/js/PilotComponents/FeaturesSection.jsx
+++ resources/js/PilotComponents/FeaturesSection.jsx
@@ -14,6 +14,33 @@ const FeaturesSection = () => {
                             Our software is tailor-made for modern digital product teams.
                         </p>
                     </div>
+                    {/* right side */}
+                    <div className="flex flex-col space-y-8 md:w-1/2">
+                        {/* item 01 start */}
+                        <div className="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row">
+                            {/* heading */}
+                            <div className="rounded-l-full bg-brightRedSupLight md:bg-transparent">
+                                <div className="flex items-center space-x-2">
+                                    <div className="px-4 py-2 text-white rounded-full md:py-1 bg-brightRed">01</div>
+                                    <h3 className="text-base font-bold md:hidden">
+                                        Track company-wide progress
+                                    </h3>
+                                </div>
+                            </div>
+                            <div>
+                                <h3 className="hidden mb-4 text-lg font-bold md:block">
+                                    Track company-wide progress
+                                </h3>
+                                <p className="text-darkGrayishBlue">
+                                    See how your day-to-day tasks fit into the wider vision. Go from
+                                    tracking progress at the milestone level all the way down to the
+                                    smallest of details. Never lose sight of the bigger picture
+                                    again.
+                                </p>
+                            </div>
+                        </div>
+                        {/* item 01 end */}
+                    </div>
                 </div>
             </section>
         </>
